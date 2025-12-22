ab9058845807d0e84d245850dc4cc6ed5937693b
Happy <happysingh1062003@gmail.com>
2025-12-23 00:04:15 +0530

    Adding Pages and Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import ResetPasswrod from "./pages/ResetPasswrod";
+import EmailVerify from "./pages/EmailVerify";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPasswrod />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/ResetPasswrod.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResetPasswrod = () => {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+    </div>
+  );
+};
+
+export default ResetPasswrod;
